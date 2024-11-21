f3b7a95dea5051a1b23362aebfe7e66cdbb64043
sidmk07 <sidmk0796@gmail.com>
2024-11-21 22:36:49 +0530

    Initial commit
    Setup backend solution with ASP.NET Core Web API

---
+++ Controllers/AttendanceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AttendanceTracker.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        // Add DbSet properties here (e.g., Students, Attendance)
+    }
+}
