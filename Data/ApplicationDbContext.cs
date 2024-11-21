14156664110a17402b091771562e8033509887a5
sidmk07 <sidmk0796@gmail.com>
2024-11-22 02:18:25 +0530

    Added database setup, models, auth and attendance API

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AttendanceTracker.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AttendanceTracker.API.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
 
-        }
-        // Add DbSet properties here (e.g., Students, Attendance)
     }
+    // Add DbSet properties here (e.g., Students, Attendance)
 }
